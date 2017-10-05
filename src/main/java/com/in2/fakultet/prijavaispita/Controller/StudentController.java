c5dcb728d6bd3b8cf9dbb16874e9f68506abc619
tagger1986 <nostojic@hotmail.com>
2017-10-06 01:51:00 +0200

    Mapiran ispis liste

--- src/main/java/com/in2/fakultet/prijavaispita/Controller/StudentController.java
+++ src/main/java/com/in2/fakultet/prijavaispita/Controller/StudentController.java
@@ -1,8 +1,13 @@
 
 package com.in2.fakultet.prijavaispita.Controller;
 
-import org.springframework.stereotype.Controller;
+import com.in2.fakultet.prijavaispita.Entity.Student;
+import com.in2.fakultet.prijavaispita.Service.StudentService;
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -10,13 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author nosto
  */
 
-@Controller
+@RestController
+@RequestMapping("/student")
 public class StudentController {
     
-    @RequestMapping(value = "/hello")
-    public String zdr(){
-        return "zdravo";
+    private StudentService studentService;
+    
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
     
     
+    @RequestMapping(method = RequestMethod.GET) 
+    public Collection<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+    
+    
+    
+    
 }
+
