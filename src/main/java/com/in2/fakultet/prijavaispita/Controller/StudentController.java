7cd63c96c42a0414396e3d615ffdf68f2963004c
tagger1986 <nostojic@hotmail.com>
2017-10-06 02:09:05 +0200

    Mapiran ispis po ID

--- src/main/java/com/in2/fakultet/prijavaispita/Controller/StudentController.java
+++ src/main/java/com/in2/fakultet/prijavaispita/Controller/StudentController.java
@@ -5,6 +5,7 @@ import com.in2.fakultet.prijavaispita.Entity.Student;
 import com.in2.fakultet.prijavaispita.Service.StudentService;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +27,15 @@ public class StudentController {
         this.studentService = studentService;
     }
     
-    
     @RequestMapping(method = RequestMethod.GET) 
     public Collection<Student> getAllStudents() {
         return studentService.getAllStudents();
+    }
+    
+   @RequestMapping (value = "/{id}", method = RequestMethod.GET)
+    public Student findById (@PathVariable("id")int id ){
+        return studentService.findById(id);
+        
     }
     
     
